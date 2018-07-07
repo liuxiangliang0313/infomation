d79affb90cdaeea17944cf1525b109d4d5ef2848
heJing <jinghedeveloper@126.com>
2018-07-07 09:24:16 +0800

    config配置文件抽取

--- manager.py
+++ manager.py
@@ -14,30 +14,10 @@ from flask_session import Session #只是用来指定session存储数据的位�
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf import CSRFProtect
+from config import Config
 
 app = Flask(__name__)
 
-#配置信息
-class Config(object):
-
-    #设置SECRET_KEY,DEBUG
-    SECRET_KEY = "fjkdjfkd"
-    DEBUG = True
-
-    #数据库配置
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@127.0.0.1:3306/information12"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    #配置redis
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    #配置session存储信息
-    SESSION_TYPE = "redis" #指定存储类型
-    SESSION_PERMANENT = False #需要过期
-    SESSION_USE_SIGNER = True #需要签名存储
-    SESSION_REDIS = redis.StrictRedis(REDIS_HOST,REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 3600*24*2 #设置session过期时间2天, 单位秒
 
 
 #从类中加载配置信息到app
