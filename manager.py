6b1795098a3173934ed2cebc5208c49cf7d618c2
heJing <jinghedeveloper@126.com>
2018-07-07 08:41:52 +0800

    数据库相关信息配置

--- manager.py
+++ manager.py
@@ -1,6 +1,30 @@
+"""
+配置信息集成:
+1.数据库配置
+2.redis配置
+3.session配置
+4.csrf配置
+5.迁移
+6.日志记录
+
+"""
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+#配置信息
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@127.0.0.1:3306/information12"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#从类中加载配置信息到app
+app.config.from_object(Config)
+
+#创建SQLAlchemy对象,关联app
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
