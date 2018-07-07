218eacf60b91c5dc92d8d802d8dc6df6e3e9c3a0
heJing <jinghedeveloper@126.com>
2018-07-07 12:09:48 +0800

    网站logo图片处理

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -7,3 +7,11 @@ from  flask import current_app,render_template
 def hello_world():
 
     return render_template('news/index.html')
+
+#设置网站logo, 每个浏览器在访问主机的时候,都会自动向该网站发送一个,GET请求, 地址: /favicon.ico
+#可以使用current_app.send_static_file('11.jpg'),自动去寻找static里面的资源
+@index_blue.route('/favicon.ico')
+def get_web_lofo():
+
+
+    return current_app.send_static_file("news/favicon.ico")
