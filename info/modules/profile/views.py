9503a619359a5c209be5f80a310b7def59b20264
liuxiangliang <liuxl0823@outlook.com>
2018-07-13 20:08:52 +0800

    个人基本信息修改,后端实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,9 @@
-from flask import g
+from flask import g, jsonify
 from flask import render_template
+from flask import request
 
 from info.utils.commons import user_login_data
+from info.utils.response_code import RET
 from . import profile_blu
 
 
@@ -10,10 +12,40 @@ from . import profile_blu
 # 请求方式:GET,POST
 # 请求参数:POST请求有参数,nick_name,signature,gender
 # 返回值:errno,errmsg
-@profile_blu.route('/base_info')
+@profile_blu.route('/base_info', methods=["GET", "POST"])
 @user_login_data
 def base_info():
-    return render_template('news/user_base_info.html',user_info=g.user.to_dict() if g.user else "")
+    """
+    1判断如果第一次进入，使用GET请求，直接返回页面即可
+    2获取参数
+    3校验参数，为空校验
+    4更新用户信息
+    5返回响应
+    :return:
+    """
+    # 1判断如果第一次进入，使用GET请求，直接返回页面即可
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', user_info=g.user.to_dict() if g.user else "")
+
+    # 2获取参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 3校验参数，为空校验
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    if not gender in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.DATAERR, errmsg="性别异常")
+
+    # 4更新用户信息
+    g.user.signature = signature
+    g.user.nick_name = nick_name
+    g.user.gender = gender
+
+    # 5返回响应
+    return jsonify(errno=RET.OK, errmsg="修改成功")
 
 
 # 展示个人中心页面
