722ef5999df1df463aaa700328f39394671778b3
liuxiangliang <liuxl0823@outlook.com>
2018-07-14 14:27:16 +0800

    展示用户头像

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -6,6 +6,28 @@ from info.utils.commons import user_login_data
 from info.utils.response_code import RET
 from . import profile_blu
 
+# 用户头像获取/设置
+# 请求路径: /user/pic_info
+# 请求方式:GET,POST
+# 请求参数:无, POST有参数,avatar
+# 返回值:GET请求: user_pci_info.html页面,data字典数据, POST请求: errno, errmsg,avatar_url
+@profile_blu.route('/pic_info', methods=['GET', 'POST'])
+@user_login_data
+def pic_info():
+    """
+    1第一次进入GET请求，直接渲染页面
+    2获取参数
+    3校验参数，为空校验
+    :return:
+    """
+    # 1第一次进入GET请求，直接渲染页面
+    if request.method == "GET":
+        return render_template("news/user_pic_info.html",user_info=g.user.to_dict())
+    # 2获取参数
+    # 3校验参数，为空校验
+
+
+
 
 # 基本资料展示
 # 请求路径: /user/base_info
---
+++ info/utils/image_storage.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from qiniu import Auth, put_file, etag
+import qiniu.config
+
+# 需要填写你的 Access Key 和 Secret Key
+from qiniu import put_data
+
+access_key = 'Hn96PWs8U8QiSfVsmIlb__CrpTJrDRqbgeASN8Qx'
+secret_key = 'chRHdO8n5XIBVxX7urzjIVgBUg8ws1KWUxFAolfu'
+# 构建鉴权对象
+q = Auth(access_key, secret_key)
+# 要上传的空间
+bucket_name = 'infomation'
+# 上传到七牛后保存的文件名
+key = None
+# 生成上传 Token，可以指定过期时间等
+token = q.upload_token(bucket_name, key, 3600)
+
+
+# 要上传文件的本地路径
+def image_storage(image_data):
+    # localfile = './bbb.jpg'
+    ret, info = put_data(token, key, image_data)
+    print(info)
+if __name__ == '__main__':
+    with open('11.jpg','rb') as file:
+        image_storage(file.read())
